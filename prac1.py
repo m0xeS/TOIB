040e0493e6dbe94adac6b8b4f981c0f7ea1981c9
m0xeS <m0xes2001@gmail.com>
2023-09-15 20:05:05 +0300

    Create prac1.py
    Done

---
+++ prac1.py
@@ -0,0 +1,28 @@
+import re
+
+def check_password(password):
+    recommendations = []
+    if len(password) < 10:
+        recommendations.append(f'add {10-len(password)} more symbols')
+    if not (re.findall(r'[A-Z]', password)):
+        recommendations.append(f'add 1 uppercase')
+    if not (re.findall(r'[0-9]', password)):
+        recommendations.append(f'add 1 digit')
+    if not (re.findall(r'\W', password)):
+        recommendations.append(f'add 1 symbol')
+    if recommendations:
+        print("Weak password. What should u do:", ", ".join(recommendations))
+    else:
+        print('Strong password')
+
+#Dpassword = input('Your password:')
+loc = input("Path to file:")
+file = open(loc, "r")
+while True:
+    password = file.readline()
+    if not password:
+        break
+    print(password.strip())
+    check_password(password.strip())
+file.close()
+    
